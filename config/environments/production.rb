dc4ab575a05c0c92cc5192b8448603b7d8af1670
Webdev4u <webdev4you@gmail.com>
2013-08-16 10:06:16 +0400

    Release

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 MotionWeb::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.assets.enabled = false
+  # config.assets.enabled = true
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -15,7 +15,7 @@ MotionWeb::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
